ce90909a23454640e335ec20830a25161b015b32
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-08-11 21:43:37 +0530

    change in hover of menu

--- src/UI/Pages/Menu.jsx
+++ src/UI/Pages/Menu.jsx
@@ -1,38 +1,39 @@
-import { Linkedin } from "lucide-react/dist/umd/lucide-react";
+import { Github, Linkedin } from "lucide-react/dist/umd/lucide-react";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen">
-    <div className="flex justify-center   ">
+    <div className="grid grid-flow-col gap-5 ">
       {/* Vertical Menu */}
-      <div className="flex  flex-4/5 md:flex-row gap-10">
-        
+      <div className=" text-center flex justify-top items-start writing-mode-vertical py-10">
+          <h3 className="block transform -rotate-90 text-2xl">Menu</h3>
+        </div>
+      <div className="grid  gap-20">
+      
 
         {/* Main Content */}
-        <div className="lg:w-3/4">
+        <div className="lg:w-96 h-48">
            
           <div>
-          <div className=" text-center flex  writing-mode-vertical py-10">
-          <h3 className="block transform -rotate-90 text-2xl">Menu</h3>
-          <button className=" sm:text-6xl text-6xl lg:text-6xl hover:bg-gray-400 rounded-lg mt-5">
+         
+          <button className=" sm:text-6xl text-6xl lg:text-6xl hover:text-7xl rounded-lg mt-5">
               <Link to="/projects">Projects</Link>
             </button>
-        </div>
             
           </div>
           <div>
-            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:bg-gray-400 rounded-lg mt-5">
+            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:text-7xl  rounded-lg mt-5">
               <Link to="/about">About</Link>
             </h2>
           </div>
           <div>
-            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:bg-gray-400 rounded-lg mt-5">
+            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:text-7xl  rounded-lg mt-5">
               <Link to="/contact">Contact</Link>
             </h2>
           </div>
           <div>
-            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:bg-gray-400 rounded-lg mt-5">
+            <h2 className=" text-2xl md:text-4xl lg:text-6xl hover:text-7xl  rounded-lg mt-5">
               <Link to="/resume">Resume</Link>
             </h2>
           </div>
@@ -40,22 +41,22 @@ const Menu = () => {
       </div>
 
       {/* Contact Info */}
-      <div className="w-4/5 flex justify-cemter items-end gap-4 p-5 font-sans">
-        <div className="flex flex-col pb-10 lg:pb-20">
+      <div className="w-96 flex justify-cemter items-end gap-4 p-5 font-sans">
+        <div className="grid gap-4">
           <div>
-            <h1 className="text-lg lg:text-3xl p-2 tracking-wide">67vivekraj@gmail.com</h1>
+            <h1 className="text-lg lg:text-3xl p-2 hover:text-4xl tracking-wide">67vivekraj@gmail.com</h1>
           </div>
           <div>
             <h1 className="text-lg lg:text-3xl p-2 tracking-wide">+91 93353 22592</h1>
           </div>
-          <div className="flex gap-8 pt-3 lg:pt-5">
+          <div className="flex gap-8 pt-3 justify-center items-center lg:pt-5">
             <div>
               <h1 className="text-lg lg:text-2xl">
                 <Linkedin />
               </h1>
             </div>
             <div>
-              <h1 className="text-lg lg:text-2xl">+</h1>
+              <h1 className="text-lg lg:text-2xl"><Github /></h1>
             </div>
           </div>
         </div>
--- src/UI/Pages/Resume.jsx
+++ src/UI/Pages/Resume.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 const Resume = () => {
   return (
-    < div className=" bg-gray-300">
-    <div>
-    <div className="flex justify-between w-full  items-center p-12 bg-black text-white ">
-        <div className="flex justify-between  w-1/6">
+    < div className=" bg-gray-300 h-screen   w-screen">
+    <div  >
+    <div className="flex bg-black text-white justify-center items-center h-24 "> 
+        <div className="grid  gap-5 grid-flow-col  ">
           <div>
             <Link to="/projects">Projects</Link>
           </div>
@@ -27,12 +27,12 @@ const Resume = () => {
         
       </div>
       
-</div>
-    <div className='h-screen mt-5 w-screen flex justify-center items-center' >    
+   </div>
+    <div className='h-screen  w-screen flex justify-center items-center' >    
     
     <object class="pdf" 
             data="https://coffee-caryn-65.tiiny.site"
-            className='h-full w-1/2 rounded-sm'
+            className='h-full  w-screen rounded-sm'
         >
     </object>
 </div>
