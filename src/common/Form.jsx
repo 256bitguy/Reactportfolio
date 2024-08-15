e7757ed5336d4cce5c08aedf685c529c4b1f87e0
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-08-15 21:32:31 +0530

    added appwrite

--- src/common/Form.jsx
+++ src/common/Form.jsx
@@ -1,11 +1,38 @@
 
 import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Field, Label, Switch } from '@headlessui/react'
+import { account, ID } from '../backend/appwrite';
+import { databases } from "../backend/appwrite";
+
+export const IDEAS_DATABASE_ID = "66be1c06002c3e47b950"; // Replace with your database ID
+export const IDEAS_COLLECTION_ID = "66be1c88000af0a94196";
 
 export default function Form() {
-  const [agreed, setAgreed] = useState(false)
+  const [data,setData]=useState({
+    firstName:'',
+    lastName:'',
+    gmailId:'',
+    contactNumber:'',
+    company:'',
+    message:''
+  });
+  function handleChange (e){
+    const {name,value}=e.target;
+    setData((prevData)=>({
+      ...prevData,[name]:value
+    }));
+  }
 
+  function handleSubmit(e){
+    databases.createDocument(
+      IDEAS_DATABASE_ID,
+      IDEAS_COLLECTION_ID,
+      ID.unique(),
+      data
+    );
+    e.preventDefault();
+    console.log(data);
+  }
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div
@@ -18,7 +45,7 @@ export default function Form() {
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Me</h2>
         
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -27,9 +54,11 @@ export default function Form() {
             <div className="mt-2.5">
               <input
                 id="first-name"
-                name="first-name"
+                name="firstName"
                 type="text"
                 autoComplete="given-name"
+                value={data.firstName}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -41,9 +70,11 @@ export default function Form() {
             <div className="mt-2.5">
               <input
                 id="last-name"
-                name="last-name"
+                name="lastName"
                 type="text"
                 autoComplete="family-name"
+                value={data.lastName}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -58,6 +89,8 @@ export default function Form() {
                 name="company"
                 type="text"
                 autoComplete="organization"
+                value={data.company}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -68,10 +101,12 @@ export default function Form() {
             </label>
             <div className="mt-2.5">
               <input
-                id="email"
-                name="email"
+                id="gmailId"
+                name="gmailId"
                 type="email"
                 autoComplete="email"
+                value={data.gmailId}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -99,10 +134,12 @@ export default function Form() {
                 />
               </div>
               <input
-                id="phone-number"
-                name="phone-number"
+                id="contactNumber"
+                name="contactNumber"
                 type="tel"
                 autoComplete="tel"
+                value={data.contactNumber}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -118,6 +155,8 @@ export default function Form() {
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
+                value={data.message}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -125,7 +164,6 @@ export default function Form() {
         </div>
         <div className="mt-10">
           <button
-            
             className="block w-full rounded-md bg-black px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Let's talk
