0204b5367355dd74488623c5886cc582a6efc273
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-08-15 22:42:01 +0530

    added proxy middleware

---
+++ src/setUpProxy.js
@@ -0,0 +1,12 @@
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api', // your API endpoint
+    createProxyMiddleware({
+      target: 'https://cloud.appwrite.io/v1',
+      changeOrigin: true,
+    })
+  );
+};
