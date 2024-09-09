ad0ef439260caf8c4776da396695b1134626cfc0
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-09-09 23:10:05 +0530

    changing the project file

--- src/UI/Pages/Projects.jsx
+++ src/UI/Pages/Projects.jsx
@@ -6,37 +6,37 @@ import { ArrowRight } from "lucide-react/dist/umd/lucide-react";
 
 const projectsDesc = [
   {
-    imgsrc: 'public/coctail.png',
+    imgsrc: '  coctail.png',
     title: "Juice Order App",
     subtitle: "An intuitive e-commerce platform for ordering a variety of fresh juices online.",
     src: "foodjuicestore",
   },
   {
-    imgsrc: 'public/chatapp.png',
+    imgsrc: '  chatapp.png',
     title: "Chat Application",
     subtitle: "A real-time messaging app designed for seamless multi-user communication.",
     src: "chatappcloud",
   },
   {
-    imgsrc: 'public/redux.png',
+    imgsrc: '  redux.png',
     title: "Redux Demo",
     subtitle: "A comprehensive demonstration of Redux for state management in React applications.",
     src: "resonant-clafoutis-d39f5e",
   },
   {
-    imgsrc: 'public/thunk.png',
+    imgsrc: '  thunk.png',
     title: "Redux-Thunk Example",
     subtitle: "Showcasing the integration of Redux-Thunk middleware for handling asynchronous actions.",
     src: "reactthunk2",
   },
   {
-    imgsrc: 'public/rdom.png',
+    imgsrc: '  rdom.png',
     title: "React Router DOM Basics",
     subtitle: "A fundamental project illustrating the core concepts and usage of React Router DOM for navigation.",
     src: "vivekrdom",
   },
   {
-    imgsrc: 'public/demo.png',
+    imgsrc: '  demo.png',
     title: "Admin Dashboard",
     subtitle: "A robust admin center for managing and overseeing institutional data effectively.",
     src: "adminakalpitkademo",
